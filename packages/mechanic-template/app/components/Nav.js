861c25b07a21c05ee54e923b0297a7df10246e96
fdoflorenzano <f.florenzano94@gmail.com>
2020-08-10 09:43:49 -0400

    :art: Replaced default route logic in Nav with prop

--- packages/mechanic-template/app/components/Nav.js
+++ packages/mechanic-template/app/components/Nav.js
@@ -5,25 +5,22 @@ import { upper } from "case";
 import css from "./Nav.css";
 import classnames from "classnames";
 
-const Nav = ({ functions }) => {
+const Nav = ({ name, functions }) => {
   const history = useHistory();
   const names = Object.keys(functions);
 
   const changeFunction = direction => {
-    const currentFunction = history.location.pathname.substring(1);
-    const currentIndex = names.indexOf(currentFunction != "" ? currentFunction : names[0]);
+    const currentIndex = names.indexOf(name);
     history.push(`/${names[(names.length + currentIndex + direction) % names.length]}`);
   };
 
   const previous = () => changeFunction(-1);
   const next = () => changeFunction(1);
 
-  const routeFunction = history.location.pathname.substring(1);
-
   return (
     <div className={classnames(css.root, css.row)}>
       <Button onClick={previous}>←</Button>
-      <span className={css.label}>{upper(routeFunction != "" ? routeFunction : names[0])}</span>
+      <span className={css.label}>{upper(name)}</span>
       <Button onClick={next}>→</Button>
     </div>
   );
