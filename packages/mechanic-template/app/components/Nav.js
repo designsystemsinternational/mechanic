7fced37fad2c2e74a25e59c05526c196c14c1496
fdoflorenzano <f.florenzano94@gmail.com>
2020-08-12 10:50:00 -0400

    :art::lipstick: Refactor over nav code and added some more CSS adjustments

--- packages/mechanic-template/app/components/Nav.js
+++ packages/mechanic-template/app/components/Nav.js
@@ -1,31 +1,22 @@
 import React from "react";
-import Button from "./input/Button";
 import { useHistory } from "react-router-dom";
-import { capital } from "case";
-import classnames from "classnames";
 import css from "./Nav.css";
 
 const Nav = ({ name, functions }) => {
   const history = useHistory();
   const names = Object.keys(functions);
 
-  const handleOnChange = ({ target }) => {
-    // console.log(value);
-    history.push(target.value);
-  };
-
-  const changeFunction = direction => {
-    const currentIndex = names.indexOf(name);
-    history.push(`/${names[(names.length + currentIndex + direction) % names.length]}`);
-  };
-
-  const next = () => changeFunction(1);
-
   return (
     <div className={css.root}>
       <span className={css.funclabel}>{name}</span>
-      <select className={css.navigationSelect} onChange={handleOnChange} name={name} value={name}>
-        <option disabled>Select...</option>
+      <select
+        className={css.navigationSelect}
+        onChange={({ target }) => history.push(target.value)}
+        name={name}
+        value={name}>
+        <option key="disabled" disabled>
+          Select...
+        </option>
         {names.map(name => (
           <option key={`route-${name}`}>{name}</option>
         ))}
