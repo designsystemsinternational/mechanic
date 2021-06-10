7cd9079991c5124c940d62168c5e030366f16c75
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2021-06-10 11:43:32 -0400

    :art::pencil: Some refactoring and comment adding

--- packages/mechanic-cli/commands/new.js
+++ packages/mechanic-cli/commands/new.js
@@ -1,6 +1,6 @@
 const fs = require("fs-extra");
 const path = require("path");
-å;
+
 const inquirer = require("inquirer");
 const ora = require("ora");
 const { mechanicSpinner } = require("./utils/spinners");
@@ -8,45 +8,61 @@ const { mechanicSpinner } = require("./utils/spinners");
 const templateDir = path.normalize(`${__dirname}/../template`);
 
 const generateProjectTemplate = async ({ project, functionName, template }) => {
+  // Start UI spinner
   const spinner = ora({
     text: "Generating mechanic project template",
     spinner: mechanicSpinner,
   }).start();
 
+  // Make new directories
   const directory = path.resolve(project);
-  const functionTemplatePath = path.normalize(
-    `${__dirname}/${template.dir}/index.js`
-  );
+  await fs.mkdir(directory); // Main
+  await fs.mkdir(path.join(directory, "functions")); // Functions folder
   const functionDir = path.join(directory, "functions", functionName);
-  await fs.mkdir(directory);
-  await fs.mkdir(path.join(directory, "functions"));
-  await fs.mkdir(functionDir);
+  await fs.mkdir(functionDir); // Design function folder
+
+  // Path of template file to copy
+  const functionTemplatePath = path.join(
+    templateDir,
+    "functions",
+    template.dir,
+    "index.js"
+  );
+
+  // Copying content promises
   await Promise.all([
+    // Copy array of files that get duplicated without change
     ...["mechanic.config.js"].map((filename) =>
       fs.copyFile(
         path.join(templateDir, filename),
         path.join(directory, filename.replace(/^_/, "."))
       )
     ),
+    // Load package.json as object and add metadata
     (async () => {
       let packageJson = await fs.readFile(
         path.join(templateDir, "package.json"),
         "utf8"
       );
       const packageObj = {
-        name: project,
+        name: project, // Adds name of project
         ...JSON.parse(packageJson),
       };
-      packageObj["dependencies"][`mechanic-engine-${template.engine}`] =
-        "^1.0.0";
+      // Add to dependencies the selected engine
+      packageObj["dependencies"][
+        `@designsystemsinternational/mechanic-engine-${template.engine}`
+      ] = "^1.0.0";
+      // Write the resulting package
       await fs.writeFile(
         path.join(directory, "package.json"),
         JSON.stringify(packageObj, null, 2)
       );
     })(),
+    // Copy template design function with different names
     fs.copyFile(functionTemplatePath, path.join(functionDir, "index.js")),
   ]);
 
+  // End UI spinner
   spinner.succeed();
 };
 
@@ -71,43 +87,43 @@ const templateOptions = [
     name: "Vanilla JS Image",
     engine: "svg",
     type: "SVG",
-    dir: "../template/functions/svgImage",
+    dir: "svgImage",
   },
   {
     name: "Vanilla JS Animation",
     engine: "svg",
     type: "SVG",
-    dir: "../template/functions/svgVideo",
+    dir: "svgVideo",
   },
   {
     name: "Vanilla JS Image",
     engine: "canvas",
     type: "Canvas",
-    dir: "../template/functions/canvasImage",
+    dir: "canvasImage",
   },
   {
     name: "Vanilla JS Animation",
     engine: "canvas",
     type: "Canvas",
-    dir: "../template/functions/canvasVideo",
+    dir: "canvasVideo",
   },
   {
     name: "React Image",
     engine: "react",
     type: "SVG",
-    dir: "../template/functions/reactImage",
+    dir: "reactImage",
   },
   {
     name: "React Animation",
     engine: "react",
     type: "SVG",
-    dir: "../template/functions/reactVideo",
+    dir: "reactVideo",
   },
   {
     name: "p5.js Animation",
     engine: "p5",
     type: "Canvas",
-    dir: "../template/functions/p5Animation",
+    dir: "p5Animation",
   },
 ];
 
@@ -147,9 +163,13 @@ const command = async (argv) => {
   // if (argv._.length > 1 && argv._[1] == "function") {
   //   console.log("FUNCTION");
   // }
+  // Ask user for customization input
   const answers = await inquirer.prompt(questions);
+  // Generate new project directory and content files
   await generateProjectTemplate(answers);
+  // Install dependencies in new project directory
   await installDependencies(answers);
+  // Done!
   console.log(`Done! Now run \`cd ${answers.project}\` and \`npm run dev\``);
 };
 
