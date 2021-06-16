3411d658ecd0d2170b0eadb1105f70a3a928a835
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2021-06-16 11:49:59 -0400

    :star: Updated react to version 17

--- packages/mechanic-app/app/pages/value-persistance.js
+++ packages/mechanic-app/app/pages/value-persistance.js
@@ -1,5 +1,96 @@
-import createPersistedState from "use-persisted-state";
-const useFunctionValues = createPersistedState("function-values");
+// https://github.com/imbhargav5/rooks/blob/master/packages/shared/useLocalstorageState.ts
+import { useState, useEffect, useCallback, useRef, Dispatch, SetStateAction } from "react";
+
+function getValueFromLocalStorage(key) {
+  if (typeof localStorage === "undefined") {
+    return null;
+  }
+  const storedValue = localStorage.getItem(key) || "null";
+  try {
+    return JSON.parse(storedValue);
+  } catch (err) {
+    console.error(err);
+  }
+  return storedValue;
+}
+
+function saveValueToLocalStorage(key, value) {
+  if (typeof localStorage === "undefined") {
+    return null;
+  }
+  return localStorage.setItem(key, JSON.stringify(value));
+}
+
+/**
+ * @param key Key of the localStorage object
+ * @param initialState Default initial value
+ */
+function initialize(key, initialState) {
+  const valueLoadedFromLocalStorage = getValueFromLocalStorage(key);
+  if (valueLoadedFromLocalStorage === null) {
+    return initialState;
+  } else {
+    return valueLoadedFromLocalStorage;
+  }
+}
+
+/**
+ * useLocalStorageState hook
+ * Tracks a value within localStorage and updates it
+ * @param {string} key - Key of the localStorage object
+ * @param {any} initialState - Default initial value
+ */
+function useLocalStorageState(key, initialState) {
+  const [value, __setValue] = useState(() => initialize(key, initialState));
+  const isUpdateFromListener = useRef(false);
+
+  useEffect(() => {
+    /**
+     * We need to ensure there is no loop of
+     * storage events fired. Hence we are using a ref
+     * to keep track of whether setValue is from another
+     * storage event
+     */
+    if (!isUpdateFromListener.current) {
+      saveValueToLocalStorage(key, value);
+    }
+  }, [value]);
+
+  const listen = useCallback(e => {
+    if (e.storageArea === localStorage && e.key === key) {
+      try {
+        isUpdateFromListener.current = true;
+        const newValue = JSON.parse(e.newValue || "null");
+        if (value !== newValue) {
+          __setValue(newValue);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, []);
+
+  // check for changes across windows
+  useEffect(() => {
+    window.addEventListener("storage", listen);
+    return () => {
+      window.removeEventListener("storage", listen);
+    };
+  }, []);
+
+  function setValue(newValue) {
+    isUpdateFromListener.current = false;
+    __setValue(newValue);
+  }
+
+  function remove() {
+    localStorage.removeItem(key);
+  }
+
+  return [value, setValue, remove];
+}
+
+// End
 
 const filterUnusedValues = (object, reference) =>
   object
@@ -12,13 +103,13 @@ const filterUnusedValues = (object, reference) =>
     : { preset: "default" };
 
 const useValues = (name, params) => {
-  const [allValues, setAllValues] = useFunctionValues({});
+  const [allValues, setAllValues] = useLocalStorageState("function-values", {});
 
   const values = filterUnusedValues(allValues[name], params);
-  const setValues = assigFunc => {
+  const setValues = assignFunc => {
     setAllValues(allValues => {
       const newFunctionValues = filterUnusedValues(allValues[name], params);
-      return Object.assign({}, allValues, { [name]: assigFunc(newFunctionValues) });
+      return Object.assign({}, allValues, { [name]: assignFunc(newFunctionValues) });
     });
   };
   return [values, setValues];
