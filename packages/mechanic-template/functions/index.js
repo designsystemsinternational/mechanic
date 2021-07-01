0dc8c958c79b44e97fb5bc38a05069721e7e6910
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2021-07-01 13:17:49 -0400

    :wastebasket: Removed now outdated template package

--- packages/mechanic-template/functions/index.js
+++
@@ -1,5 +0,0 @@
-import { setUp } from "@designsystemsinternational/mechanic-utils";
-
-const functionsContext = require.context("../functions", true, /^(.{2,})\/index\.js$/);
-
-setUp(functionsContext);
