edee124e51e7b76a7452f818fca2f36cdde31dc1
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2021-09-13 17:55:57 -0300

    :bug: Fixed bug for unhandled default values case

--- packages/core/app/pages/utils/presets.js
+++ packages/core/app/pages/utils/presets.js
@@ -21,7 +21,7 @@ const addPresetsAsSources = (inputValue, inputName, presets, inputs, values) => 
   } else if (
     values.hasOwnProperty("preset") &&
     values.preset != NO_PRESET_VALUE &&
-    presets[values.preset].hasOwnProperty(inputName)
+    (values.preset === DEFAULT_PRESET_VALUE || presets[values.preset].hasOwnProperty(inputName))
   ) {
     sources.push({ preset: NO_PRESET_VALUE });
   }
