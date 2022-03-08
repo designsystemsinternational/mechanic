24fca60bf95afb00ac5e0e14fbb54271a2987bbb
Fernando Florenzano Hernandez <f.florenzano94@gmail.com>
2022-03-08 12:12:19 -0300

    refactor(app): Tweaked not found view and added URL to home

--- packages/core/app/components/NotFound.js
+++ packages/core/app/components/NotFound.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import * as css from "./NotFound.module.css";
 
 export const NotFound = props => {
+  console.log({ props });
   return (
     <div className={css.root}>
-      <p>Not found</p>
+      <p>
+        URL not found! Go <Link to="/">home</Link>.
+      </p>
     </div>
   );
 };
