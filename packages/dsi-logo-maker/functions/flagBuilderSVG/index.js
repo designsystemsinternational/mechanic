cc3de6b68d48caf7d520f834519795c7e59f35c4
Lucas Dino Nolte <hello@lucas-nolte.com>
2022-04-19 15:00:42 +0200

    feat: add deterministic flag builder design function

---
+++ packages/dsi-logo-maker/functions/flagBuilderSVG/index.js
@@ -0,0 +1,125 @@
+import React, { useEffect, useMemo } from "react";
+
+import { getColors, flagNames } from "../../utils/graphics";
+import { buildSeeds } from "../../utils/seeds";
+import { buildBlocks } from "../../utils/flagBlocks";
+
+export const handler = ({ inputs, mechanic }) => {
+  const {
+    width,
+    height,
+    colorMode,
+    flag,
+    firstColor,
+    secondColor,
+    thirdColor,
+    seedMode,
+    seedText,
+    seedForDistribution,
+    seedForOffset,
+  } = inputs;
+
+  const colors = getColors(colorMode, flag, [
+    firstColor,
+    secondColor,
+    thirdColor,
+  ]);
+
+  const { seedDistribution, seedOffset } = buildSeeds(seedMode, seedText, {
+    seedForDistribution,
+    seedForOffset,
+  });
+
+  const blocks = useMemo(() => {
+    return buildBlocks({
+      width,
+      height,
+      seedDistribution,
+      seedOffset,
+      colors,
+    });
+  }, [width, height, seedDistribution, seedOffset, colors]);
+
+  useEffect(() => {
+    mechanic.done();
+  }, []);
+
+  return (
+    <svg width={width} height={height}>
+      {blocks.map((block, i) => (
+        <rect
+          key={i}
+          x={block.x}
+          y={block.y}
+          width={block.w}
+          height={block.h}
+          fill={block.color}
+        />
+      ))}
+    </svg>
+  );
+};
+
+export const inputs = {
+  width: {
+    type: "number",
+    default: 600,
+  },
+  height: {
+    type: "number",
+    default: 400,
+  },
+  seedMode: {
+    type: "text",
+    options: ["From Text", "Set Numbers"],
+    default: "From Text",
+  },
+  seedText: {
+    type: "text",
+    default: "Seed text",
+    editable: (inputs) => inputs.seedMode === "From Text",
+  },
+  seedForDistribution: {
+    type: "number",
+    default: 0,
+    editable: (inputs) => inputs.seedMode === "Set Numbers",
+  },
+  seedForOffset: {
+    type: "number",
+    default: 0,
+    editable: (inputs) => inputs.seedMode === "Set Numbers",
+  },
+  colorMode: {
+    type: "text",
+    options: ["Random Flag", "Pick Flag", "Custom Colors"],
+    default: "Random Flag",
+  },
+  flag: {
+    type: "text",
+    options: flagNames,
+    default: flagNames[0],
+    editable: (inputs) => inputs.colorMode === "Pick Flag",
+  },
+  firstColor: {
+    type: "color",
+    model: "hex",
+    default: "#11457e",
+    editable: (inputs) => inputs.colorMode === "Custom Colors",
+  },
+  secondColor: {
+    type: "color",
+    model: "hex",
+    default: "#d7141a",
+    editable: (inputs) => inputs.colorMode === "Custom Colors",
+  },
+  thirdColor: {
+    type: "color",
+    model: "hex",
+    default: "#f1f1f1",
+    editable: (inputs) => inputs.colorMode === "Custom Colors",
+  },
+};
+
+export const settings = {
+  engine: require("@mechanic-design/engine-react"),
+};
---
+++ packages/dsi-logo-maker/utils/flagBlocks.js
@@ -0,0 +1,69 @@
+// Predetermined sets of color distributions
+// min: quarter line
+// max: entire line
+const colorDistributions = [
+  [0.75, 0.5, 0.75],
+  [0.5, 0.75, 0.75],
+  [0.5, 0.5, 1],
+  [0.75, 0.75, 0.5],
+  [0.75, 0.25, 1],
+  [0.5, 1, 0.5],
+  [0.375, 0.875, 0.75],
+  [0.75, 0.375, 0.875],
+];
+
+const offsets = [-0.5, -0.375, -0.25, 0, 0.25, 0.375, 0.5];
+const pickItemBySeed = (arr, n) => arr[n % arr.length];
+
+export const buildBlocks = ({
+  width = 620,
+  height = 370,
+  seedDistribution = 0,
+  seedOffset = 0,
+  colors,
+}) => {
+  const colorDistribution = pickItemBySeed(
+    colorDistributions,
+    seedDistribution
+  ).map((d) => d * width);
+
+  const offset = pickItemBySeed(offsets, seedOffset) * width;
+
+  // TODO: Refactor to be more functional and have less side effects
+  const blocks = () => {
+    let x = offset;
+    let y = 0;
+
+    const blocks = [];
+
+    const buildBlock = (_x, _y, _w, color) => {
+      const x = _x < 0 ? _x + width : _x;
+      const y = _x < 0 ? _y + height / 2 : _y;
+      const remainder = x + _w - width;
+      const w = remainder > 0 ? _w - remainder : _w;
+
+      blocks.push({ x, y, w, h: height / 2, color });
+
+      if (remainder > 0) {
+        // If there is a remainder it's safe to set x to 0
+        return buildBlock(0, y > 0 ? 0 : height / 2, remainder, color);
+      } else {
+        return {
+          x: x + w,
+          y,
+        };
+      }
+    };
+
+    for (let i = 0; i < colorDistribution.length; i++) {
+      const item = colorDistribution[i];
+      const result = buildBlock(x, y, item, colors[i].background);
+      x = result.x;
+      y = result.y;
+    }
+
+    return blocks;
+  };
+
+  return blocks();
+};
