8080c8ffe78600dbc057476dd4719cd372cefb28
Lucas Nolte <lucas@designsystems.international>
2022-09-16 12:41:05 +0200

    fix: move .prettierrc to root

--- packages/dsi-logo-maker/functions/textCanvas/index.js
+++ packages/dsi-logo-maker/functions/textCanvas/index.js
@@ -1,34 +1,18 @@
 import { getColors } from "../../utils/graphics";
-import {
-  computeBaseBricks,
-  computeBlockGeometry,
-  computeBlock,
-} from "../../utils/blocks";
+import { computeBaseBricks, computeBlockGeometry, computeBlock } from "../../utils/blocks";
 import { drawBlock } from "../../utils/blocks-canvas";
 
 export const handler = ({ inputs, mechanic }) => {
-  const {
-    width,
-    ratio,
-    text,
-    columns: cols,
-    rows,
-    colors: colorsString,
-    offset,
-  } = inputs;
+  const { width, ratio, text, columns: cols, rows, colors: colorsString, offset } = inputs;
 
-  const words = text.split(" ").map((s) => s.toUpperCase());
+  const words = text.split(" ").map(s => s.toUpperCase());
   const colors = getColors("Custom Colors", null, colorsString.split(","));
   const height = Math.floor((width / ratio) * rows);
 
   const blockGeometry = computeBlockGeometry(width, height, rows, cols);
   const baseBricks = computeBaseBricks(words, blockGeometry.fontSize);
 
-  const block = computeBlock(
-    blockGeometry,
-    baseBricks,
-    Math.floor(offset * baseBricks.length)
-  );
+  const block = computeBlock(blockGeometry, baseBricks, Math.floor(offset * baseBricks.length));
   const position = { x: 0, y: 0 };
 
   const canvas = document.createElement("canvas");
@@ -47,7 +31,7 @@ export const inputs = {
   width: {
     type: "number",
     default: 500,
-    min: 100,
+    min: 100
   },
   ratio: {
     type: "number",
@@ -55,27 +39,27 @@ export const inputs = {
     max: 20,
     slider: true,
     min: 6,
-    step: 1,
+    step: 1
   },
   text: {
     type: "text",
-    default: "Whatever you want",
+    default: "Whatever you want"
   },
   columns: {
     type: "number",
     default: 13,
     min: 1,
-    step: 1,
+    step: 1
   },
   rows: {
     type: "number",
     default: 2,
     min: 1,
-    step: 1,
+    step: 1
   },
   colors: {
     type: "text",
-    default: "#11457e,#d7141a,#f1f1f1",
+    default: "#11457e,#d7141a,#f1f1f1"
   },
   offset: {
     type: "number",
@@ -83,21 +67,21 @@ export const inputs = {
     min: 0,
     max: 1,
     step: 0.05,
-    slider: true,
-  },
+    slider: true
+  }
 };
 
 export const presets = {
   bigger: {
     width: 500,
-    ratio: 9,
+    ratio: 9
   },
   evenBigger: {
     width: 500,
-    ratio: 9,
-  },
+    ratio: 9
+  }
 };
 
 export const settings = {
-  engine: require("@mechanic-design/engine-canvas"),
+  engine: require("@mechanic-design/engine-canvas")
 };
