ac9c21df85aa648188dd2c67696dd23ae321f9af
Rune Madsen <rune@runemadsen.com>
2022-09-16 10:30:59 +0200

    adding suggestion

--- packages/create-mechanic/function-templates/react-video/index.js
+++ packages/create-mechanic/function-templates/react-video/index.js
@@ -1,22 +1,27 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 
-export const handler = ({ inputs, mechanic }) => {
+// We pass in the frameCount variable, so Mechanic is now responsible for
+// running the handler function on every frame. This makes it possible for
+// us to create a timeline that when you scrub simply re-renders the handler
+// by passing in the updated frameCount.
+// For the React engine, this means that the component will render on every frame,
+// but a solution is to pass the frameCount to a react-spring and then re-render with
+// a memoed compomnent.
+export const handler = ({ inputs, mechanic, frameCount }) => {
   const { width, height, text, color1, color2, radiusPercentage, turns } =
     inputs;
-
   const center = [width / 2, height / 2];
   const radius = ((height / 2) * radiusPercentage) / 100;
   const angle = useRef(0);
 
-  const isPlaying = useRef(true);
-  const frameCount = useDrawLoop(isPlaying.current);
-
   useEffect(() => {
     if (angle.current < turns * 360) {
+      // The next frame will not advance until the frame() function is called.
+      // This makes it possible to load assets and other things before drawing the first frame.
       mechanic.frame();
       angle.current += 360 / 100;
-    } else if (isPlaying.current) {
-      isPlaying.current = false;
+    } else {
+      // When done() is called, the design function will not run again.
       mechanic.done();
     }
   }, [frameCount]);
@@ -54,78 +59,52 @@ export const handler = ({ inputs, mechanic }) => {
 
 export const inputs = {
   width: {
-    type: "number",
-    default: 400,
+    type: 'number',
+    default: 400
   },
   height: {
-    type: "number",
-    default: 300,
+    type: 'number',
+    default: 300
   },
   text: {
-    type: "text",
-    default: "mechanic",
+    type: 'text',
+    default: 'mechanic'
   },
   color1: {
-    type: "color",
-    model: "hex",
-    default: "#E94225",
+    type: 'color',
+    model: 'hex',
+    default: '#E94225'
   },
   color2: {
-    type: "color",
-    model: "hex",
-    default: "#002EBB",
+    type: 'color',
+    model: 'hex',
+    default: '#002EBB'
   },
   radiusPercentage: {
-    type: "number",
+    type: 'number',
     default: 40,
     min: 0,
     max: 100,
-    slider: true,
+    slider: true
   },
   turns: {
-    type: "number",
-    default: 3,
-  },
+    type: 'number',
+    default: 3
+  }
 };
 
 export const presets = {
   medium: {
     width: 800,
-    height: 600,
+    height: 600
   },
   large: {
     width: 1600,
-    height: 1200,
-  },
+    height: 1200
+  }
 };
 
 export const settings = {
-  engine: require("@mechanic-design/engine-react"),
-  animated: true,
-};
-
-const useDrawLoop = (isPlaying) => {
-  const raf = useRef();
-  const [frameCount, setFrameCount] = useState(0);
-
-  useEffect(() => {
-    cancelAnimationFrame(raf.current);
-
-    if (!isPlaying) {
-      return;
-    }
-
-    const draw = () => {
-      setFrameCount((cur) => cur + 1);
-      raf.current = requestAnimationFrame(draw);
-    };
-
-    draw();
-
-    return () => {
-      cancelAnimationFrame(raf.current);
-    };
-  }, [isPlaying]);
-
-  return frameCount;
+  engine: require('@mechanic-design/engine-react'),
+  animated: true
 };
