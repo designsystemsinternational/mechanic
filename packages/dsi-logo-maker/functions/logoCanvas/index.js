fe8cb755f68bfe2818fc8e770b8f5d229289199d
Lucas Nolte <lucas@designsystems.international>
2022-10-06 15:32:20 +0200

    chore: refactor logoCanvas

--- packages/dsi-logo-maker/functions/logoCanvas/index.js
+++ packages/dsi-logo-maker/functions/logoCanvas/index.js
@@ -1,11 +1,11 @@
-import { getColors, flagNames } from "../../utils/graphics";
+import { getColors, flagNames } from '../../utils/graphics';
 import {
   computeBaseBricks,
   computeBlockGeometry,
   computeBlock,
-} from "../../utils/blocks";
-import { loadOpentypeFont } from "../../utils/opentype";
-import { drawBlock } from "../../utils/blocks-canvas";
+} from '../../utils/blocks';
+import { loadOpentypeFont } from '../../utils/opentype';
+import { drawBlock } from '../../utils/blocks-canvas';
 
 export const handler = async ({ inputs, mechanic }) => {
   const {
@@ -22,7 +22,7 @@ export const handler = async ({ inputs, mechanic }) => {
 
   const rows = 2;
   const cols = 13;
-  const words = ["DESIGN", "SYSTEMS", "INTERNATIONAL"];
+  const words = ['DESIGN', 'SYSTEMS', 'INTERNATIONAL'];
   const height = Math.floor((width / ratio) * rows);
   const font = await loadOpentypeFont(fontMode);
 
@@ -41,29 +41,27 @@ export const handler = async ({ inputs, mechanic }) => {
   );
   const position = { x: 0, y: 0 };
 
-  const canvas = document.createElement("canvas");
+  const canvas = document.createElement('canvas');
   canvas.width = width;
   canvas.height = height;
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext('2d');
 
   ctx.save();
   ctx.clearRect(0, 0, blockGeometry.width, blockGeometry.height);
   drawBlock(ctx, { position, block, colors });
   ctx.restore();
-  mechanic.done(
-    canvas,
-    colorMode !== "Custom Colors" ? `${flag}-${offset}` : null
-  );
+
+  return canvas;
 };
 
 export const inputs = {
   width: {
-    type: "number",
+    type: 'number',
     default: 500,
     min: 100,
   },
   ratio: {
-    type: "number",
+    type: 'number',
     default: 9,
     max: 20,
     slider: true,
@@ -71,44 +69,44 @@ export const inputs = {
     step: 1,
   },
   fontMode: {
-    type: "text",
+    type: 'text',
     options: {
-      "F Grotesk Thin": "FGroteskThin-Regular.otf",
-      "F Grotesk": "FGrotesk-Regular.otf",
+      'F Grotesk Thin': 'FGroteskThin-Regular.otf',
+      'F Grotesk': 'FGrotesk-Regular.otf',
     },
-    default: "F Grotesk Thin",
+    default: 'F Grotesk Thin',
   },
   colorMode: {
-    type: "text",
-    options: ["Random Flag", "Pick Flag", "Custom Colors"],
-    default: "Random Flag",
+    type: 'text',
+    options: ['Random Flag', 'Pick Flag', 'Custom Colors'],
+    default: 'Random Flag',
   },
   flag: {
-    type: "text",
+    type: 'text',
     options: flagNames,
     default: flagNames[0],
-    editable: (inputs) => inputs.colorMode === "Pick Flag",
+    editable: (inputs) => inputs.colorMode === 'Pick Flag',
   },
   firstColor: {
-    type: "color",
-    model: "hex",
-    default: "#11457e",
-    editable: (inputs) => inputs.colorMode === "Custom Colors",
+    type: 'color',
+    model: 'hex',
+    default: '#11457e',
+    editable: (inputs) => inputs.colorMode === 'Custom Colors',
   },
   secondColor: {
-    type: "color",
-    model: "hex",
-    default: "#d7141a",
-    editable: (inputs) => inputs.colorMode === "Custom Colors",
+    type: 'color',
+    model: 'hex',
+    default: '#d7141a',
+    editable: (inputs) => inputs.colorMode === 'Custom Colors',
   },
   thirdColor: {
-    type: "color",
-    model: "hex",
-    default: "#f1f1f1",
-    editable: (inputs) => inputs.colorMode === "Custom Colors",
+    type: 'color',
+    model: 'hex',
+    default: '#f1f1f1',
+    editable: (inputs) => inputs.colorMode === 'Custom Colors',
   },
   offset: {
-    type: "number",
+    type: 'number',
     default: 0,
     min: 0,
     max: 1,
@@ -129,5 +127,5 @@ export const presets = {
 };
 
 export const settings = {
-  engine: require("@mechanic-design/engine-canvas"),
+  engine: require('@mechanic-design/engine-canvas'),
 };
