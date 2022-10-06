cf0572f0d18b83bd938d842211a04918a4395b0c
Lucas Nolte <lucas@designsystems.international>
2022-10-06 15:18:25 +0200

    chore: refactor flagBuilderCanvas

--- packages/dsi-logo-maker/functions/flagBuilderCanvas/index.js
+++ packages/dsi-logo-maker/functions/flagBuilderCanvas/index.js
@@ -1,6 +1,6 @@
-import { getColors, flagNames } from "../../utils/graphics";
-import { buildSeeds } from "../../utils/seeds";
-import { buildBlocks } from "../../utils/flagBlocks";
+import { getColors, flagNames } from '../../utils/graphics';
+import { buildSeeds } from '../../utils/seeds';
+import { buildBlocks } from '../../utils/flagBlocks';
 
 export const handler = ({ inputs, mechanic }) => {
   const {
@@ -36,80 +36,81 @@ export const handler = ({ inputs, mechanic }) => {
     colors,
   });
 
-  const canvas = document.createElement("canvas");
+  const canvas = document.createElement('canvas');
   canvas.width = width;
   canvas.height = height;
 
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext('2d');
 
   blocks.forEach((block) => {
     ctx.fillStyle = block.color;
     ctx.fillRect(block.x, block.y, block.w, block.h);
   });
 
-  mechanic.done(canvas);
+  return canvas;
 };
 
 export const inputs = {
   width: {
-    type: "number",
+    type: 'number',
     default: 600,
   },
   height: {
-    type: "number",
+    type: 'number',
     default: 400,
   },
   seedMode: {
-    type: "text",
-    options: ["From Text", "Set Numbers"],
-    default: "From Text",
+    type: 'text',
+    options: ['From Text', 'Set Numbers'],
+    default: 'From Text',
   },
   seedText: {
-    type: "text",
-    default: "Seed text",
-    editable: (inputs) => inputs.seedMode === "From Text",
+    type: 'text',
+    default: 'Seed text',
+    editable: (inputs) => inputs.seedMode === 'From Text',
   },
   seedForDistribution: {
-    type: "number",
+    type: 'number',
     default: 0,
-    editable: (inputs) => inputs.seedMode === "Set Numbers",
+    editable: (inputs) => inputs.seedMode === 'Set Numbers',
   },
   seedForOffset: {
-    type: "number",
+    type: 'number',
     default: 0,
-    editable: (inputs) => inputs.seedMode === "Set Numbers",
+    editable: (inputs) => inputs.seedMode === 'Set Numbers',
   },
   colorMode: {
-    type: "text",
-    options: ["Random Flag", "Pick Flag", "Custom Colors"],
-    default: "Random Flag",
+    type: 'text',
+    options: ['Random Flag', 'Pick Flag', 'Custom Colors'],
+    default: 'Random Flag',
   },
   flag: {
-    type: "text",
+    type: 'text',
     options: flagNames,
     default: flagNames[0],
-    editable: (inputs) => inputs.colorMode === "Pick Flag",
+    editable: (inputs) => inputs.colorMode === 'Pick Flag',
   },
   firstColor: {
-    type: "color",
-    model: "hex",
-    default: "#11457e",
-    editable: (inputs) => inputs.colorMode === "Custom Colors",
+    type: 'color',
+    model: 'hex',
+    default: '#11457e',
+    editable: (inputs) => inputs.colorMode === 'Custom Colors',
   },
   secondColor: {
-    type: "color",
-    model: "hex",
-    default: "#d7141a",
-    editable: (inputs) => inputs.colorMode === "Custom Colors",
+    type: 'color',
+    model: 'hex',
+    default: '#d7141a',
+    editable: (inputs) => inputs.colorMode === 'Custom Colors',
   },
   thirdColor: {
-    type: "color",
-    model: "hex",
-    default: "#f1f1f1",
-    editable: (inputs) => inputs.colorMode === "Custom Colors",
+    type: 'color',
+    model: 'hex',
+    default: '#f1f1f1',
+    editable: (inputs) => inputs.colorMode === 'Custom Colors',
   },
 };
 
 export const settings = {
-  engine: require("@mechanic-design/engine-canvas"),
+  engine: require('@mechanic-design/engine-canvas'),
+  mode: 'static',
 };
